ec1c04116307835f465f6a2cfeecac5eccb97e23
Julien Champseix <julien.champseix@gmail.com>
2024-12-04 10:19:17 +0100

    day 4 star 2

--- days/day4.gd
+++ days/day4.gd
@@ -42,3 +42,27 @@ func _on_run_pressed(prefix: String='data', suffix: String=''):
             total += check_pattern(lines, width, height, x, y, +1, 0)
             total += check_pattern(lines, width, height, x, y, +1, +1)
     print(total)
+
+func _on_run_2_pressed(prefix: String='data', suffix: String=''):
+    var lines = self.read_input(prefix, suffix)
+    var height = len(lines)
+    var width = len(lines[0])
+    var total = 0
+    for y in range(1, height-1):
+        for x in range(1, width-1):
+            total += check_x(lines, width, height, x, y)
+    print(total)
+
+
+func check_x(lines: PackedStringArray, width: int, height: int, x: int, y: int) -> int:
+    if lines[y][x] != 'A':
+        return 0
+    var is_mas = lines[y-1][x-1] == 'M' and lines[y+1][x+1] == 'S'
+    var is_sam = lines[y-1][x-1] == 'S' and lines[y+1][x+1] == 'M'
+    if not is_mas and not is_sam:
+        return 0
+    is_mas = lines[y+1][x-1] == 'M' and lines[y-1][x+1] == 'S'
+    is_sam = lines[y+1][x-1] == 'S' and lines[y-1][x+1] == 'M'
+    if not is_mas and not is_sam:
+        return 0
+    return 1        
