faaa1800808beb58ef8c12bf6ba0ef5a12aa9b70
Julien Champseix <julien.champseix@gmail.com>
2024-12-05 10:22:53 +0100

    day 5 star 1

---
+++ days/day5.gd
@@ -0,0 +1,75 @@
+extends "res://days/day.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    self.day_number = 5
+    self.day_suffix = ''
+    super._ready()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
+
+func parse_input(lines: PackedStringArray):
+    var dependencies = {}
+    var printable = []
+    var i = 0
+    while lines[i]:
+        var d = lines[i].split('|')
+        if d[1] not in dependencies:
+            dependencies[d[1]] = []
+        dependencies[d[1]].append(d[0])
+        i += 1
+    i += 1
+    while i < len(lines):
+        var p = lines[i].split(',')
+        printable.append(p)
+        i += 1
+    return [dependencies, printable]
+
+func print_page(printable, printed, p, s, dependencies):
+    if p in printed:
+        return
+    if p not in printable:
+        return
+    var slocal = [p]
+    if p in dependencies:
+        for x in dependencies[p]:
+            print_page(printable, printed, x, slocal, dependencies)
+    slocal.reverse()
+    s.append_array(slocal)
+
+func process(lines: PackedStringArray):
+    var parsed_input = parse_input(lines)
+    var dependencies = parsed_input[0]
+    var printables = parsed_input[1]
+    var total = 0
+    var adding = []
+    for printable in printables:
+        var printed = []
+        for p in printable:
+            var s = []
+            print_page(printable, printed, p, s, dependencies)
+            printed.append_array(s)
+        # print(printable, ' vs ', printed)
+        if str(printable) == str(printed):
+            var item = int(printable[int((len(printable))/2)])
+            adding.append(item)
+            total += item
+    print(' + '.join(adding), ' = ', total)
+
+func _on_run_pressed(prefix: String='data', suffix: String=''):
+    var lines = self.read_input(prefix, suffix)
+    process(lines)
+
+
+func process2(lines: PackedStringArray):
+    print('not implemented yet')
+
+
+func _on_run_2_pressed(prefix: String='data', suffix: String=''):
+    var lines = self.read_input(prefix, suffix)
+    process2(lines)
+    
