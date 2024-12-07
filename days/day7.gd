2e986947c3019c185cf81e98ebaaa65bb4560a6d
Julien Champseix <julien.champseix@gmail.com>
2024-12-07 18:58:57 +0100

    day 7 star 1

---
+++ days/day7.gd
@@ -0,0 +1,50 @@
+extends "res://days/day.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    self.day_number = 7
+    self.day_suffix = ''
+    super._ready()
+    print('EXAMPLE')
+    var input_lines = self.read_input('example')
+    process_lines(input_lines)
+
+    print('DATA')
+    input_lines = self.read_input('data')
+    process_lines(input_lines)
+
+func tryoperators(s, cc, v):
+    if not cc:
+        return s == v
+    var c = cc[0]
+    var a = s + c
+    if a <= v and tryoperators(a, cc.slice(1), v):
+        return true
+
+    var m = s * c
+    if m <= v and tryoperators(m, cc.slice(1), v):
+        return true
+    return false
+
+
+func process_lines(lines: PackedStringArray):
+    var total = 0
+    for line in lines:
+        if not line:
+            continue
+        var x = line.split(':')
+        var v = int(x[0])
+        x = x[1].split(' ')
+        var components = []
+        for c in x:
+            if not c:
+                continue
+            components.append(int(c))
+        var s = components[0]
+        if tryoperators(s, components.slice(1), v):
+            #print('found: ', v)
+            total = total + v
+    # 880584941917 is too low
+    # 882304362421 
+    print('total: ', total)
